d1a08b4a0b0ff905af8dd300f637a52adec86f37
mohammad <mohammadpakivand@gmail.com>
2023-02-11 13:15:26 +0330

    single food added

--- routes/foods.js
+++ routes/foods.js
@@ -1,4 +1,5 @@
 const foods = require('../db/foods.json');
+const singleFood = require('../db/single-food.json');
 
 module.exports = {
     getFoods(req, res) {
@@ -6,5 +7,17 @@ module.exports = {
             total: 68,
             result: foods
         });
+    },
+
+    getFood(req, res) {
+        const id = req.params.id;
+        if (id) {
+            res.status(200).send({
+                data: singleFood
+            });
+        } else {
+            res.status(404).send({message: "Not found!"});
+        }
+        
     },
 }
